cbb749f624679ef077d0a71c41d2a4bd061dc1e0
eder_arcaya <neguinavonts12@gmail.com>
2023-09-13 00:46:40 -0300

    Arquivos enviados

---
+++ src/S01/Ex1.java
@@ -0,0 +1,18 @@
+package S01;
+
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Qual seu sobrenome?");
+        String sobrenome = scanner.nextLine();
+        System.out.println("Qual seu nome?");
+        String nome = scanner.nextLine();
+        String nomeCompleto = nome + " " + sobrenome;
+        int letters = sobrenome.replaceAll("\\s","").length() + nome.replaceAll("\\s","").length();
+
+        System.out.println("Seu nome é: " + nomeCompleto);
+        System.out.println("A quantidade de letras em seu nome é: " + letters);
+    }
+}
